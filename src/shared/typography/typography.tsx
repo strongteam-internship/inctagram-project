18921fe57b484da0fe88c1d093a794fe8f1f1da4
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-11-25 15:53:55 +0300

    done

--- src/shared/typography/typography.tsx
+++ src/shared/typography/typography.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
+import { ComponentPropsWithoutRef, ElementType } from 'react'
+import s from './typography.module.scss'
 
 type AvoidElements = 'a' | 'h1' | 'h2' | 'h3' | 'p'
-type AvoidVariants =
+export type AvoidVariants =
   | 'Large'
   | 'H1'
   | 'H2'
@@ -12,63 +13,63 @@ type AvoidVariants =
   | 'regular_text_14'
   | 'medium_text_14'
   | 'bold_text_14'
-  | 'small_text_14'
-  | 'Semi_bold_small_text_14'
+  | 'small_text_12'
+  | 'semi_bold_small_text_12'
   | 'regular_link'
   | 'small_link'
 
 const Variants: Record<AvoidVariants, { tag: AvoidElements; className: string }> = {
   Large: {
     tag: 'p',
-    className: '',
+    className: s.Large,
   },
   H1: {
     tag: 'h1',
-    className: '',
+    className: s.H1,
   },
   H2: {
     tag: 'h2',
-    className: '',
+    className: s.H2,
   },
   H3: {
     tag: 'h3',
-    className: '',
+    className: s.H3,
   },
   regular_text_16: {
     tag: 'p',
-    className: '',
+    className: s.regular_text_16,
   },
   bold_text_16: {
     tag: 'p',
-    className: '',
+    className: s.bold_text_16,
   },
   regular_text_14: {
     tag: 'p',
-    className: '',
+    className: s.regular_text_14,
   },
   medium_text_14: {
     tag: 'p',
-    className: '',
+    className: s.medium_text_14,
   },
   bold_text_14: {
     tag: 'p',
-    className: '',
+    className: s.bold_text_14,
   },
-  small_text_14: {
+  small_text_12: {
     tag: 'p',
-    className: '',
+    className: s.small_text_12,
   },
-  Semi_bold_small_text_14: {
+  semi_bold_small_text_12: {
     tag: 'p',
-    className: '',
+    className: s.semi_bold_small_text_12,
   },
   regular_link: {
     tag: 'a',
-    className: '',
+    className: s.regular_link,
   },
   small_link: {
     tag: 'a',
-    className: '',
+    className: s.small_link,
   },
 } as const
 
@@ -76,24 +77,26 @@ type VariantMapping = typeof Variants
 
 type Props<V extends keyof VariantMapping> = {
   variant?: V
-  children: ReactNode
+  children: string
+  align: 'right' | 'left' | 'center'
 } & (V extends 'regular_link' | 'small_link' ? { href: string } : {}) &
   Omit<ComponentPropsWithoutRef<VariantMapping[V]['tag']>, 'children'>
 
 export function Typography<V extends keyof VariantMapping>({
   children,
   variant,
+  align,
   ...props
 }: Props<V>) {
   let Component
-  let className
+  let className = `${s[align]} `
 
   if (variant) {
     Component = Variants[variant].tag as ElementType
-    className = Variants[variant].className
+    className += Variants[variant].className
   } else {
     Component = 'p'
-    className = 'regular_text_16'
+    className += 'regular_text_16'
   }
 
   return (
@@ -102,107 +105,3 @@ export function Typography<V extends keyof VariantMapping>({
     </Component>
   )
 }
-
-//Large
-// Font: Inter
-// Font weight: 600
-// Font size: 26pxpx
-// Row height: 36px
-// Letter spacing: 0px
-// Align: Align left
-
-//H1
-// Font: Inter
-// Font weight: 700
-// Font size: 20pxpx
-// Row height: 36px
-// Letter spacing: 0px
-// Align: Align left
-
-//H2
-// Font: Inter
-// Font weight: 700
-// Font size: 18pxpx
-// Row height: 24px
-// Letter spacing: 0px
-// Align: Align left
-
-//H3
-// Font: Inter
-// Font weight: 600
-// Font size: 16pxpx
-// Row height: 24px
-// Letter spacing: 0px
-// Align: Align left
-
-//regular_text_16
-// Font: Inter
-// Font weight: 400
-// Font size: 16pxpx
-// Row height: 24px
-// Letter spacing: 0%
-// Align: Align left
-
-//Bold_text_16
-// Font: Inter
-// Font weight: 700
-// Font size: 16pxpx
-// Row height: 24px
-// Letter spacing: 0%
-// Align: Align left
-
-//regular_text 14
-// Font: Inter
-// Font weight: 400
-// Font size: 14pxpx
-// Row height: 24px
-// Letter spacing: 0%
-// Align: Align left
-
-//Medium_text 14
-// Font: Inter
-// Font weight: 500
-// Font size: 14pxpx
-// Row height: 24px
-// Letter spacing: 0%
-// Align: Align left
-
-//bold_text 14
-// Font: Inter
-// Font weight: 700
-// Font size: 14pxpx
-// Row height: 24px
-// Letter spacing: 0%
-// Align: Align left
-
-//small_text
-// Font: Inter
-// Font weight: 400
-// Font size: 12pxpx
-// Row height: 16px
-// Letter spacing: 0%
-// Align: Align left
-
-//Semi-bold small_text
-// Font: Inter
-// Font weight: 600
-// Font size: 12pxpx
-// Row height: 16px
-// Letter spacing: 0%
-// Align: Align left
-
-//regular_link
-// Font: Inter
-// Font weight: 400
-// Font size: 14pxpx
-// Row height: 24px
-// Letter spacing: 0%
-// Align: Align left
-
-//small_link
-// Font: Inter
-// Font weight: 400
-// Font size: 12pxpx
-// Row height: 16px
-// Letter spacing: 0%
-// Align: Align left
