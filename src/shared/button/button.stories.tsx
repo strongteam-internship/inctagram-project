f200ea1d88372c6503c3fe302ad576bfaa1d4f84
It_ru_girl <ksu606@ya.ru>
2024-11-27 16:46:13 +0300

    add button

---
+++ src/shared/button/button.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button } from './button'
+
+const meta = {
+    title: 'Components/Button',
+    component: Button,
+    tags: ['autodocs'],
+    argTypes: {
+        variant: {
+            options: ['primary', 'secondary', 'outline', 'link'],
+            control: { type: 'radio' },
+        },
+    },
+} satisfies Meta<typeof Button>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+    args: {
+        variant: 'primary',
+        children: 'Primary Button',
+        disabled: false,
+    },
+}
+
+export const Secondary: Story = {
+    args: {
+        variant: 'secondary',
+        children: 'Secondary Button',
+        disabled: false,
+    },
+}
+export const Outline: Story = {
+    args: {
+        variant: 'outline',
+        children: 'Outline Button',
+        disabled: false,
+    },
+}
+
+export const TextButton: Story = {
+    args: {
+        variant: 'link',
+        children: 'Tertiary Button',
+        disabled: false,
+    },
+}
