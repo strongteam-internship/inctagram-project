ac59bc7d22fd9cc59cade153f1ed424dd328d70e
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2024-12-08 00:58:14 +0300

    add middleware in store

--- src/aplication/store/store.ts
+++ src/aplication/store/store.ts
@@ -1,9 +1,12 @@
 import { appSlice } from '@/aplication/model/app/appSlice'
+import { authApi } from '@/features/auth/api/authApi'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(authApi.middleware),
   reducer: {
     app: appSlice,
+    [authApi.reducerPath]: authApi.reducer,
   },
 })
 
