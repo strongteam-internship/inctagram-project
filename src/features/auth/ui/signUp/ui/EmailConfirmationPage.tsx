63d408c1c5709ca705e9e4ff49be9d1624833270
ttony62 <1988anton62@gmail.com>
2025-01-09 22:42:16 +0300

    add EmailConfirmationPage

---
+++ src/features/auth/ui/signUp/ui/EmailConfirmationPage.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { Button } from '@/shared/button/button'
+import { Typography } from '@/shared/typography/typography'
+
+import s from './EmailConfirmationPage.module.scss'
+
+export default function EmailConfirmationPage() {
+  return (
+    <div className={s.page}>
+      <div className={s.container}>
+        <Typography align={'center'} variant={'H1'}>
+          Congratulations!
+        </Typography>
+        <Typography align={'center'} variant={'regular_text_16'}>
+          Your email has been confirmed
+        </Typography>
+        <Button>Sign In</Button>
+      </div>
+    </div>
+  )
+}
