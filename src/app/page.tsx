faaf2e377f1daa12b140be88f6d43ec27f284b42
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-01-10 19:17:10 +0300

    add router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 'use client'
+import { useEffect } from 'react'
+
+import { useAppSelector } from '@/application/hooks/hooks'
+import { useGetMeQuery } from '@/features/auth/api/authApi'
 import { Typography } from '@/shared/typography/typography'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+  const isLogIn = useAppSelector(state => state.app.isLoggedIn)
+
+  useEffect(() => {
+    if (!isLogIn) {
+      router.push('/signin')
+    }
+  }, [isLogIn, router])
+
   return (
     <div>
       TEST
