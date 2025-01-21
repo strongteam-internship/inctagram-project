bca9265f92debb2284bb052ced13a45060e8fd69
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-01-21 23:06:31 +0300

    fix guard

--- src/application/services/AuthGuard/AuthGuard.tsx
+++ src/application/services/AuthGuard/AuthGuard.tsx
@@ -2,9 +2,8 @@
 
 import { ReactNode, useEffect } from 'react'
 
-import { useAppDispatch, useAppSelector } from '@/application/hooks/hooks'
-import { setIsLoggedIn } from '@/application/model/app/appSlice'
-import { useRouter } from 'next/navigation'
+import { useAppSelector } from '@/application/hooks/hooks'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 type AuthGuardProps = {
   children: ReactNode
@@ -12,17 +11,17 @@ type AuthGuardProps = {
 
 export function AuthGuard({ children }: AuthGuardProps) {
   const router = useRouter()
-  const isLogIn = useAppSelector(state => state.app.isLoggedIn)
-  const dispatch = useAppDispatch()
+  const isLoggedIn = useAppSelector(state => state.app.isLoggedIn)
+  const searchParams = useSearchParams()
+  const code = searchParams.get('code')
 
   useEffect(() => {
-    const authToken = localStorage.getItem('token')
-
-    if (!authToken) {
-      dispatch(setIsLoggedIn(false))
+    if (code) {
+      router.push('/auth/registration-confirmation')
+    } else if (!isLoggedIn) {
       router.push('/auth/signin')
     }
-  }, [router])
+  }, [router, code, isLoggedIn])
 
   return <>{children}</>
 }
