b1755d4e634973317cbe8e3f948988adc8d57315
And4ik <And4ik11@gmail.com>
2025-01-24 22:22:44 +0100

    Navbar commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,30 @@ import React from 'react'
 
 import { AppProvider } from '@/application/services/appProvider/appProvider'
 import { Header } from '@/widgets/header/Header'
+import { Navbar } from '@/widgets/navbar/Navbar'
 
 import '@/styles/index.scss'
 
-import styles from './main.module.scss'
+import s from './main.module.scss'
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const isAuthenticated = true
+
   return (
     <html lang={'en'} suppressHydrationWarning>
       <body>
-        <div className={styles.main}>
-          <Header isAuthenticated notificationsCount={1} />
-          <AppProvider>{children}</AppProvider>
+        <div className={s.main}>
+          <Header isAuthenticated={isAuthenticated} notificationsCount={1} />
+          <div className={s.layoutContent}>
+            {isAuthenticated && <Navbar />}
+            <div className={s.contentArea}>
+              <AppProvider>{children}</AppProvider>
+            </div>
+          </div>
         </div>
       </body>
     </html>
