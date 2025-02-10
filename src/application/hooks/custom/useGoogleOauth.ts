d897bf6f5c5db366257271748f59ee033926af36
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-02-10 17:28:34 +0300

    fix

--- src/application/hooks/custom/useGoogleOauth.ts
+++ src/application/hooks/custom/useGoogleOauth.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useMemo } from 'react'
 
 import { useGetGoogleOAuth2Mutation } from '@/features/auth/api/authApi'
 import { useRouter, useSearchParams } from 'next/navigation'
@@ -8,11 +8,12 @@ export function useGoogleOAuthLogin() {
   const searchParams = useSearchParams()
   const [getGoogleOAuthLogin] = useGetGoogleOAuth2Mutation()
 
-  const code = searchParams.get('code')
+  const code = useMemo(() => searchParams.get('code'), [searchParams])
   const scope = 'email'
   const CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID
   const redirectUrl = 'http://localhost:3000'
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&response_type=code&redirect_uri=${redirectUrl}&client_id=${CLIENT_ID}`
+  const redirectUrl1 = 'https://strong-interns.top'
+  const url = `https://accounts.google.com/o/oauth2/v2/auth?scope=${scope}&response_type=code&redirect_uri=${redirectUrl}&client_id=${CLIENT_ID}&prompt=consent`
 
   function login() {
     router.push(url)
@@ -23,7 +24,7 @@ export function useGoogleOAuthLogin() {
       if (code) {
         console.log('code', code)
         try {
-          const res = await getGoogleOAuthLogin({ code: code, redirectUrl: redirectUrl })
+          const res = await getGoogleOAuthLogin({ code: code, redirectUrl: redirectUrl1 })
 
           console.log('res', res)
         } catch (error) {
