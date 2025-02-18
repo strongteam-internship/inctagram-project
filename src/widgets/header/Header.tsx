b229bde70567529d5d51fbcbb9877cc82edb3dbc
And4ik <And4ik11@gmail.com>
2025-02-18 23:30:35 +0100

    fix: add isLoggedIn for correct render

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from 'react'
 
+import { useAppSelector } from '@/application/hooks/hooks'
 import { Option, Select } from '@/shared/Select/Select'
 import { Button } from '@/shared/button/button'
 import FlagEn from '@/shared/input/icons/FlagEn'
@@ -13,21 +14,22 @@ import Link from 'next/link'
 import styles from './Header.module.scss'
 
 type HeaderProps = {
-  isAuthenticated: boolean
   notificationsCount?: number
 }
 
-export const Header: FC<HeaderProps> = ({ isAuthenticated, notificationsCount = 0 }) => {
+export const Header: FC<HeaderProps> = ({ notificationsCount = 0 }) => {
+  const isLoggedIn = useAppSelector(state => state.app.isLoggedIn)
+
   return (
     <header className={styles.header}>
       <Link href={'/'}>
         <Typography variant={'Large'}>Inctagram</Typography>
       </Link>
       <div className={styles.headerActions}>
-        {isAuthenticated && (
+        {isLoggedIn && (
           <div className={styles.notification}>
             <NotificationIcon />
-            {notificationsCount && notificationsCount > 0 && (
+            {notificationsCount > 0 && (
               <div className={styles.count}>
                 {notificationsCount > 9 ? '9+' : notificationsCount}
               </div>
@@ -50,7 +52,7 @@ export const Header: FC<HeaderProps> = ({ isAuthenticated, notificationsCount = 
             </Option>
           </Select>
         </div>
-        {!isAuthenticated && (
+        {!isLoggedIn && (
           <>
             <Button as={Link} className={styles.login} href={'/login'} variant={'link'}>
               Log in
