bcfc82412f2066303bd748c2a8b4ecae42694c99
ttony62 <1988anton62@gmail.com>
2025-02-19 21:52:39 +0300

    fix: LogOut.tsx

--- src/features/auth/ui/logOut/LogOut.tsx
+++ src/features/auth/ui/logOut/LogOut.tsx
@@ -2,35 +2,18 @@
 
 import { useState } from 'react'
 
-import { useAppDispatch } from '@/application/hooks/hooks'
-import { setIsLoggedIn } from '@/application/model/app/appSlice'
+import { useLogOut } from '@/application/hooks/custom/useLogOut'
 import SvgLogOut from '@/assets/svg/icons/components/LogOut'
-import { useGetLogOutMutation } from '@/features/auth/api/authApi'
 import { Button } from '@/shared/button/button'
 import { Modal } from '@/shared/modal'
 import { Typography } from '@/shared/typography/typography'
-import { useRouter } from 'next/navigation'
 
 import s from './LogOut.module.scss'
 
 export function LogOut() {
   const [isOpen, setIsOpen] = useState(false)
-  const dispatch = useAppDispatch()
-  const [getLogOut] = useGetLogOutMutation()
-  const router = useRouter()
 
-  const handleLogOut = () => {
-    getLogOut()
-      .unwrap()
-      .then(() => {
-        dispatch(setIsLoggedIn(false))
-        router.push('/api/v1/auth/login')
-        localStorage.removeItem('token')
-      })
-      .catch(error => {
-        throw new Error('Unexpected error')
-      })
-  }
+  const { handleLogOut } = useLogOut()
 
   return (
     <>
@@ -48,12 +31,7 @@ export function LogOut() {
         <Typography variant={'regular_text_16'}>{`Are you really want to log out of your account “
         email@.com”`}</Typography>
         <div className={s.buttons}>
-          <Button
-            onClick={() => {
-              handleLogOut()
-            }}
-            variant={'outline'}
-          >
+          <Button onClick={handleLogOut} variant={'outline'}>
             Yes
           </Button>
           <Button onClick={() => setIsOpen(false)}>No</Button>
