4ce685d1084accf901d6007b2e6f4cacb60be80d
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-02-20 23:44:56 +0300

    feat: добавил функцию проверки типа

---
+++ src/application/api/types/typeGuards/typeGuards.ts
@@ -0,0 +1,17 @@
+import { AppErrorResponse } from '@/application/api/types/types'
+import { ErrorMessage } from '@/features/auth/utils/types/types'
+
+export function isErrorResponse<T>(errorRes: unknown): errorRes is AppErrorResponse<T> {
+  return (
+    typeof errorRes === 'object' &&
+    errorRes !== null &&
+    'data' in errorRes &&
+    typeof errorRes.data === 'object' &&
+    errorRes.data !== null &&
+    'error' in errorRes.data &&
+    typeof errorRes.data.error === 'string' &&
+    'messages' in errorRes.data &&
+    'status' in errorRes &&
+    typeof errorRes.status === 'number'
+  )
+}
