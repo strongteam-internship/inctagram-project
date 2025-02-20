d67249e5ee439bb44750fe1de55d62a3b8983c31
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-02-20 23:43:19 +0300

    refactor: измеил хранение токена

--- src/application/api/baseQueryWithReauth.ts
+++ src/application/api/baseQueryWithReauth.ts
@@ -2,22 +2,16 @@ import { setAppStatus, setIsLoggedIn } from '@/application/model/app/appSlice'
 import { showToastError } from '@/features/auth/api/showToastError'
 import { BaseQueryFn, FetchArgs, FetchBaseQueryError, fetchBaseQuery } from '@reduxjs/toolkit/query'
 import { Mutex } from 'async-mutex'
-import { deleteCookie, getCookie, setCookie } from 'cookies-next/client'
 
 type RefreshTokenResponse = {
   accessToken: string
-}
-
-type RefreshResult = {
-  data?: RefreshTokenResponse
-  error?: FetchBaseQueryError
 }
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_BASE_URL,
   credentials: 'include',
   prepareHeaders: headers => {
-    const accessToken = getCookie('accessToken')
+    const accessToken = localStorage.getItem('accessToken')
 
     if (accessToken) {
       headers.set('Authorization', `Bearer ${accessToken}`)
@@ -40,18 +34,17 @@ export const baseQueryWithReauth: BaseQueryFn<
   if (result.error && result.error.status === 401) {
     if (!mutex.isLocked()) {
       const release = await mutex.acquire()
-      let refreshResult: RefreshResult = { data: undefined, error: undefined }
 
       try {
         api.dispatch(setAppStatus('loading')) // 1. Устанавливаем статус загрузки
-        refreshResult = await baseQuery(
+        const refreshResult = await baseQuery(
           { method: 'POST', url: '/api/v1/auth/update-tokens/' },
           api,
           extraOptions
         )
 
         if (refreshResult.data?.accessToken) {
-          setCookie('accessToken', refreshResult.data.accessToken) // 2. Сохраняем accessToken
+          localStorage.setItem('accessToken', refreshResult.data.accessToken) // 2. Сохраняем accessToken
           result = await baseQuery(args, api, extraOptions) // 3. Повторяем запрос
 
           if (!result.error) {
@@ -64,7 +57,7 @@ export const baseQueryWithReauth: BaseQueryFn<
           api.dispatch(setAppStatus('error'))
           showToastError('Ошибка авторизации. Пожалуйста, войдите снова.')
           setTimeout(() => {
-            deleteCookie('accessToken')
+            localStorage.removeItem('accessToken')
             api.dispatch(setIsLoggedIn(false))
           }, 2000)
           // window.location.href = '/auth/signin' - управление редиректами будет на уровне маршрутов или компонент с помощью useRouter
@@ -74,7 +67,9 @@ export const baseQueryWithReauth: BaseQueryFn<
         showToastError('Ошибка повторной авторизации. Пожалуйста, войдите снова.')
       } finally {
         release()
-        api.dispatch(setAppStatus(refreshResult.data?.accessToken ? 'idle' : 'error')) // Проверяем результат и устанавливаем статус
+        const token = localStorage.getItem('accessToken')
+
+        api.dispatch(setAppStatus(token ? 'idle' : 'error')) // Проверяем результат и устанавливаем статус
       }
     } else {
       await mutex.waitForUnlock()
