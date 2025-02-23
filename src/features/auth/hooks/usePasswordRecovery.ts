9ee71fd1763c191a459801e6acffca08c5553fa5
Pavel <47242671+Pavel-Retunskih@users.noreply.github.com>
2025-02-23 16:05:44 +0300

    refactor: вынес логику в хук

---
+++ src/features/auth/hooks/usePasswordRecovery.ts
@@ -0,0 +1,40 @@
+import { useEffect } from 'react'
+import { UseFormSetError } from 'react-hook-form'
+
+import * as process from 'node:process'
+
+import { ErrorMessage } from '@/application/api/types/types'
+import { isErrorResponse } from '@/application/utils/typeGuards/typeGuards'
+import { useGetPasswordRecoveryMutation } from '@/features/auth/api/authApi'
+import { ForgotPasswordSchemaType } from '@/features/auth/utils/validationRules/zodSchema'
+
+type Props = {
+  errorHandler: UseFormSetError<ForgotPasswordSchemaType>
+  successHandler: (email: string) => void
+}
+
+export function usePasswordRecovery({ errorHandler, successHandler }: Props) {
+  const [getPassword, { error, isError, isSuccess }] = useGetPasswordRecoveryMutation()
+  const baseUrl = process.env.NEXT_PUBLIC_PRODUCT_URL as string
+  let emailRes = ''
+
+  useEffect(() => {
+    if (isError) {
+      if (isErrorResponse<ErrorMessage<string>>(error)) {
+        errorHandler('email', { message: error.data.messages as string })
+      }
+    }
+  }, [isError, error, errorHandler])
+  function getPasswordRecovery({ email, recaptcha }: { email: string; recaptcha: string }) {
+    emailRes = email
+    getPassword({ baseUrl, email, recaptcha })
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      successHandler(emailRes)
+    }
+  }, [])
+
+  return { getPasswordRecovery }
+}
